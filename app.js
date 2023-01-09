cc08bb7c705fe6d17129aa951480ca19a925d4bc
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-01-09 15:30:13 +0300

    js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // ---- ---- Const ---- ---- //
 const stars = document.querySelectorAll('.stars i');
+const starsNone = document.querySelector('.rating-box');
 
 // ---- ---- Stars ---- ---- //
 stars.forEach((star, index1) => {
   star.addEventListener('click', () => {
-    console.log(index1);
     stars.forEach((star, index2) => {
-      console.log(index2);
       // ---- ---- Active Star ---- ---- //
       index1 >= index2
         ? star.classList.add('active')
